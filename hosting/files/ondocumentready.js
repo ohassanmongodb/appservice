d7c2c392f307cceaf23dfe4b9ba26ecf69d22d8b
o.hassan <o.hassan@mongodb.com>
2023-04-04 14:56:21 +0200

    no message

--- hosting/files/ondocumentready.js
+++ hosting/files/ondocumentready.js
@@ -1,9 +1,19 @@
+const APP_ID = 'application-1-bzgbi';
+const ATLAS_SERVICE = 'mongodb-atlas';
+const app = new Realm.App({id: APP_ID});
 (function(){
 
-    setInterval(myFunction, 5000);
+    setInterval(position, 5000);
 })();
 
 
-function myFunction() {
-    console.log("Hello World!");
- }
+function position() {
+    const mongodb = app.currentUser.mongoClient(ATLAS_SERVICE);
+    navigator.geolocation.getCurrentPosition(position => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude; 
+        console.log(latitude);
+        console.log(longitude);
+        collMovies = mongodb.db("position").collection("currentPosition");
+        collMovies.insertOne({latitude:latitude,longitude:longitude})
+     }); }
