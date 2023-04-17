a4d5269ab547cbba563317073d08476d9249e4aa
o.hassan <o.hassan@mongodb.com>
2023-04-17 14:44:51 +0200

    asdsad

--- hosting/files/msg.js
+++ hosting/files/msg.js
@@ -1,3 +1,18 @@
-(function(){
-    console.log("hello");
-})();
+const msg = ()=>{
+    const APP_ID = 'application-1-bzgbi';
+    const ATLAS_SERVICE = 'mongodb-atlas';
+    const app = new Realm.App({id: APP_ID});
+    app.logIn(Realm.Credentials.anonymous());
+    const mongodb = app.currentUser.mongoClient(ATLAS_SERVICE);
+
+    var text = $(".input-block").val();
+    console.log(text);
+    var collection = mongodb.db("msg").collection("msg");
+    navigator.geolocation.getCurrentPosition(position => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude; 
+        var msg = {"message":text,position:[latitude,longitude]};
+        console.log(msg);
+        collection.insertOne(msg);
+     });
+}
