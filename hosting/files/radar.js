3fd06568925dfc0814db62cd0f5d02fb5286fa74
o.hassan <o.hassan@mongodb.com>
2023-04-18 13:11:33 +0200

    no message

--- hosting/files/radar.js
+++ hosting/files/radar.js
@@ -12,20 +12,14 @@ const reload = () =>{
     navigator.geolocation.getCurrentPosition(position => {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude; 
-        var match = {
-            $match:{
-                position: {
-                    $near:  [latitude,longitude],
-                    $maxDistance: 10
-                }
+        var messages = collection.find({
+            position:
+            {
+                $near:  [latitude,longitude],
+                $maxDistance: 10
             }
-        };
-        var limit = {
-            $limit:5
-        };
-        var pipeline = [match,limit];
-        var messages1 = collection.aggregate(pipeline);
-        messages1.then(ms =>{
+        });
+        messages.then(ms =>{
             console.log(ms);
             ms.forEach(m =>{
                 $("#results").append("<div class='alert alert-danger'>"+m.message+"</div>")
